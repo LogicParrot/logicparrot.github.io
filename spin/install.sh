e88acc44a634064cd8163714221592c24eed1231
SafwatHalaby <5030488+SafwatHalaby@users.noreply.github.com>
2018-01-30 22:41:10 +0200

    root pass

--- spin/install.sh
+++ spin/install.sh
@@ -22,8 +22,8 @@ missingPrograms()
 {
 	echo "$1 is missing. I need to install all dependencies first. Auto-executing this command:"
 	echo ">sudo apt-get install bison gcc make wget perl"
-	echo "(please type your ROOT password if asked. Also, Internet connection is needed):"
-	sudo apt-get install byacc flex gcc make wget perl || error "Failed to install dependencies. Sorry! Try again, or try running above command manually"
+	echo "(please type your password if asked. Also, Internet connection is needed):"
+	sudo apt-get install byacc flex gcc make wget perl || error "Failed to install dependencies. Sorry! Try again, or try running as root, or try installing above dependencies manually"
 	echo "Dependency installation finished!"
 }
 
